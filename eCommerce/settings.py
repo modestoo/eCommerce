d98edba9de2e93aa6f4d7c60deeedabb56438df5
Augusto Modesto <augusto.modesto@live.com>
2018-01-27 11:58:54 -0200

    Preparando arquivos de configuração para deploy (heroku)

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,3 +129,13 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'staticfiles')
 ]
 '''
+
+# Atualiza configuração do banco de dados para o postgree no servidor de produção (Heroku)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
